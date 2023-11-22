74737e2e67676be60d8cbeff87eccb7bc61285c0
erick <erickungu82@gmail.com>
2023-11-22 12:34:12 +0300

    Add the custom button props to the component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,8 @@
 'use client'
 import Image from 'next/image'
+import { CustomButtonProps } from '@/types'
 
-
-function CustomButton() {
+function CustomButton({title,containerStyles,handleClick}: CustomButtonProps) {
   return (
     <button
       disabled ={false}

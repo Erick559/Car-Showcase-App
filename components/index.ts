d0e342094cc491195d92b252ccaaa53b9206bd4a
erick <erickungu82@gmail.com>
2023-11-22 15:52:04 +0300

    Add navbar and footer to the index file

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,12 @@
 import Hero from './hero'
 import CustomButton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
 
 export{
     Hero,
-    CustomButton
+    CustomButton,
+    Navbar,
+    Footer
 }

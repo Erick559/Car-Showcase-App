5aa5a4cb5aa3fd7aeb48cf1a4a9a492ec06e790d
Erick <erickungu82@gmail.com>
2023-12-03 11:05:38 +0300

    Create fetchCars api to fetch the cars data from rapid Api

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,11 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=lamborghini';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8fd300d674msh629d5bc74711582p1f7ba3jsn67d446c2822b',
+export async function fetchCars(){
+    const headers = {
+        'X-RapidAPI-Key': '8fd300d674msh629d5bc74711582p1f7ba3jsn67d446c2822b',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+    }
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',{headers: headers})
+    const result = await response.json()
+
+    return result
 }

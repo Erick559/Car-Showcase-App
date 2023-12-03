668242e4cc245bf90991d31b7a353348b53839dc
erick <erickungu82@gmail.com>
2023-12-03 09:44:43 +0300

    Add the searchManufacturer components. Add use state and handle search function to handle filtering

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client'
+
+import { SearchManufacturer } from "."
+import { useState } from "react"
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState('')
+  const HandleSearch = () => {}
   return (
-    <div>SearchBar</div>
+    <form className='searchbar' onSubmit={HandleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer = {manufacturer}
+          setManufacturer = {setManufacturer} />
+      </div>
+
+    </form>
   )
 }
 

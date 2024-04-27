6e3c756e655ad52eebef876accafaaec96fa82d9
Erick559 <erickungu82@gmail.com>
2024-04-27 12:47:29 +0300

    feat: Render the car images.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -3,6 +3,7 @@ import { Dialog,Transition } from '@headlessui/react'
 
 import { CarProps } from '@/types'
 import Image from 'next/image'
+import { fetchCarImages } from '@/utils'
 
 interface CarDetailsProps {
     isOpen:boolean,
@@ -47,17 +48,17 @@ const CarDetails = ({isOpen,closeModal,car}:CarDetailsProps) => {
 
                             <div className='flex-1 flex flex-col gap-3'>
                                 <div className='relative w-full h-24 bg-pattern bg-center rounded-lg'>
-                                    <Image src='/hero.png' alt='car model' fill priority className='object-contain'/>
+                                    <Image src={fetchCarImages(car)} alt='car model' fill priority className='object-contain'/>
                                 </div>
                                 <div className='flex gap-3'>
                                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                        <Image src='/hero.png' alt='car model' fill priority className='object-contain'/>
+                                        <Image src={fetchCarImages(car,'29')} alt='car model' fill priority className='object-contain'/>
                                     </div>
                                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                        <Image src='/hero.png' alt='car model' fill priority className='object-contain'/>
+                                        <Image src={fetchCarImages(car,'33')} alt='car model' fill priority className='object-contain'/>
                                     </div>
                                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                                        <Image src='/hero.png' alt='car model' fill priority className='object-contain'/>
+                                        <Image src={fetchCarImages(car,'13')} alt='car model' fill priority className='object-contain'/>
                                     </div>
                                 </div>
                             </div>

2499290fd435301bd240c8b62caa11ae12a0b2fd
Erick559 <erickungu82@gmail.com>
2024-04-27 10:24:07 +0300

    feat: Create the car details modal.

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import { CarProps } from '@/types'
 import { CustomButton } from '.'
 import { CalculateCarRent } from '@/utils'
+import CarDetails from './CarDetails'
+
 
 interface CarCardProps {
     car: CarProps,
@@ -71,6 +73,7 @@ const CarCard = ({car}:CarCardProps) => {
                 
             </div>
         </div>
+        <CarDetails isOpen={isOpen} closeModal={()=> {setIsOpen(false)}} car={car} />
     </div>
   )
 }

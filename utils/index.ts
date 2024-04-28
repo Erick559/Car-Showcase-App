5806ec2454feaf53161bfeefd78a300ca8746530
Erick559 <erickungu82@gmail.com>
2024-04-28 10:47:30 +0300

    feat: Destructure the filters from the search params and pass them as queries to the api endpoint.

--- utils/index.ts
+++ utils/index.ts
@@ -1,15 +1,25 @@
-import { CarProps } from "@/types"
+import { CarProps, FilterProps } from "@/types"
 
-export async function fetchCars(){
-    const headers = {
-        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_CAR_KEY,
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, model, limit, fuel } = filters;
+  
+    // Set the required headers for the API request
+    const headers: HeadersInit = {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_CAR_KEY || "",
+      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    };
+  
+    // Set the required headers for the API request
+    const response = await fetch(
+       `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+      {
+        headers: headers,
+      }
+    );
 
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{headers: headers})
-    const result = await response.json()
+    const result = await response.json();
 
-    return result
+    return result;
 }
 
 export const CalculateCarRent = (city_mpg: number, year:number) => {
